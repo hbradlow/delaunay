ff3b05a3bd80555a04408d08af436e6a2fa93613
Henry Bradlow <hbradlow@berkeley.edu>
2013-05-01 22:12:05 -0700

    Fixed setup file

--- ec2setup.sh
+++ ec2setup.sh
@@ -1,11 +1,9 @@
-sudo apt-get update
-sudo apt-get install git
-sudo apt-get install gcc
-sudo apt-get install pip
-sudo apt-get install python-dev
-sudo pip install numpy
+apt-get install git
 git clone git://github.com/hbradlow/delaunay.git
 cd delaunay
 wget http://www.cs.berkeley.edu/~jrs/input/ttimeu10000.node.gz
+gunzip ttimeu10000.node.gz
 wget http://www.cs.berkeley.edu/~jrs/input/ttimeu100000.node.gz
+gunzip ttimeu100000.node.gz
 wget http://www.cs.berkeley.edu/~jrs/input/ttimeu1000000.node.gz
+gunzip ttimeu1000000.node.gz
